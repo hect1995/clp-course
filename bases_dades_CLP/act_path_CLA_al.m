4e4e87449334211d69406828a42c849457422ae4
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-09-28 09:33:00 +0200

    Modify database script to work locally with MATLAB

--- bases_dades_CLP/act_path_CLA_al.m
+++ bases_dades_CLP/act_path_CLA_al.m
@@ -1,17 +1,18 @@
 
-base='H:\soft';
+%base='H:\soft';
+base='/home/albert/git/clp-lab/bases_dades_CLP';
 
-base=[base,'\BDatos'];
+base=[base,'/BDatos'];
 path(path,base)
 
-path(path,[base,'\MicroArray']);
-path(path,[base,'\spam']);
-path(path,[base,'\zipcode']);
-path(path,[base,'\titanic']);
-path(path,[base,'\sheart']);
-path(path,[base,'\phoneme']);
-path(path,[base,'\Voces']);
-path(path,[base,'\Brain']);
-path(path,[base,'\soft_CLP']);
+path(path,[base,'/MicroArray']);
+path(path,[base,'/spam']);
+path(path,[base,'/zipcode']);
+path(path,[base,'/titanic']);
+path(path,[base,'/sheart']);
+path(path,[base,'/phoneme']);
+path(path,[base,'/Voces']);
+path(path,[base,'/Brain']);
+path(path,[base,'/soft_CLP']);
 
 clear base;
