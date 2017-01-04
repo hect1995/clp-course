acc96fcc4114c7405abb35d1d671b8e3ea340e79
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2017-01-04 18:57:52 +0100

    Finished section 2.1
    Also saved some figures

--- Lab_7/src/mainscript.m
+++ Lab_7/src/mainscript.m
@@ -1,5 +1,9 @@
 %% Exercise 2.1 script of the K-Means classifier
 clear
+close all
+
+% Switch to activate or deactivate the plotting of all classifiers
+plot_clusters = 1;
 
 %% Section 1
 % Generate DB
@@ -19,39 +23,68 @@ probabilities = probabilities./sum(probabilities);
 % Draw clusters
 scatter(DB(1,:), DB(2,:))%, hold on
 
-%% TODO: Fix Section 3 to catch all J values from CLP_Kmeans 
+%% Section 3
 % Classify with K-Means clustering
-J = zeros(9,1);
+
+% Preallocate results from the classifier
+J = cell(9,1);
+minimized_J = zeros(9,1);
+
 trace1 = zeros(9,1);
 trace2 = zeros(9,1);
 
-for K=2:10
-    [Centroides, Labels, n , J(K-1), trace1(K-1), trace2(K-1), ...
-        Sw(:,:,K-1), Sb(:,:,K-1)] = CLP_Kmeans(DB(1:d, :),K, d);    
-    
-%     % Plot evolution of the centroids
-%     for i=1:K
-%         c = rand(1,3);
-%         scatter(Centroides(1,i,:), Centroides(2,i,:), 'x', ...
-%             'MarkerEdgeColor', c/sum(c))
-%         line(reshape(Centroides(1,i,:),[1,n]), ...
-%             reshape(Centroides(2,i,:),[1,n]), 'Color', c/sum(c))
-%     end
-%     hold off
+Sw = zeros(2,2,9);
+Sb = zeros(2,2,9);
 
-    % Plot DB with color labeling
-    figure, hold on
-    for i=1:K
-        c = rand(1,3);
-        scatter(DB(1,Labels==i), DB(2,Labels==i), ...
-            'MarkerEdgeColor', c/sum(c))
-        scatter(Centroides(1,i,:), Centroides(2,i,:), ...
-            'x', 'MarkerEdgeColor', 1 - c/sum(c))
+for K=2:10
+    [Centroides, Labels, n , J{K-1}, trace1(K-1), trace2(K-1), ...
+        Sw(:,:,K-1), Sb(:,:,K-1)] = CLP_Kmeans(DB(1:d, :),K, d);
+    
+    minimized_J(K-1) = J{K-1}(end);
+    
+    if plot_clusters
+        % Plot DB with color labeling
+        figure, hold on
+        for i=1:K
+            c = rand(1,3);
+            scatter(DB(1,Labels==i), DB(2,Labels==i), ...
+                'MarkerEdgeColor', c/sum(c))
+            scatter(Centroides(1,i,:), Centroides(2,i,:), ...
+                'x', 'MarkerEdgeColor', 1 - c/sum(c))
+        end
     end
 end
 
-figure
-semilogy(2:10, J), grid on
-
-%% TODO Section 4
+%% Section 4
 % Analyze classification metrics
+
+figure, hold on
+plot(2:10, minimized_J)
+grid on
+title('J function')
+hold off
+
+figure, hold on
+plot(2:10, trace1)
+grid on
+title('Trace 1')
+hold off
+
+figure, hold on
+plot(2:10, trace2)
+grid on
+title('Trace 2')
+hold off
+
+% Trace 2 is great, Trace 1 not good, because it decays constantly. It does so
+% because as we increase the number of clusters, each of them is more compact,
+% so the "within" metric improves.
+%
+% The J function behaves sometimes roughly like Trace 1
+%
+% We want to minimize Trace 1 and maximize Trace 2
+%
+% Trace 2 increases greatly when different clusters are classified as such,
+% while Trace 1 does slightly decrease in the same situation.
+%
+% Because of this, Trace 2 is the best metric
